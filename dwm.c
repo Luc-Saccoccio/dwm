f07135d982aa8741c38324a11271cde2cec39380
Luc-Saccoccio <lucsaccoccio@disroot.org>
2020-12-29 23:09:59 +0100

    Need to change the active monitor indicator code

--- dwm.c
+++ dwm.c
@@ -843,6 +843,7 @@ drawbar(Monitor *m)
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
 		if (occ & 1 << i)
+			// TODO: Modify this to have a nicer indicator
 			drw_rect(drw, x + boxw, 0, w - ( 2 * boxw + 1), boxw,
 			    m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
 			    urg & 1 << i);
